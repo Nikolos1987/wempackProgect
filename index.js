fb2575ec8845c92101226915320273baf28b4255
nikita <nikolos04051987@gmail.com>
2023-10-14 20:00:55 +0700

    fix:addwebpack

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const soundData = [
     {
-        name: "rain",
-        path: "files/assets/sounds/rain.mp3",
-        chect: false,
-        audio: new Audio("files/assets/sounds/rain.mp3"),
-        value:50
-    },
-    {
         name: "summer",
         path: "files/assets/sounds/summer.mp3",
         chect: false,
         audio: new Audio("files/assets/sounds/summer.mp3"),
-        value:100
+        value: 100
     },
+    {
+        name: "rain",
+        path: "files/assets/sounds/rain.mp3",
+        chect: false,
+        audio: new Audio("files/assets/sounds/rain.mp3"),
+        value: 50
+    },
+
     {
         name: "winter",
         path: "files/assets/sounds/winter.mp3",
         chect: false,
         audio: new Audio("files/assets/sounds/winter.mp3"),
-        value:100
+        value: 100
     },
 ]
 const startAudio = () => {
     soundData.forEach(e => {
         if (e.chect) {
             e.audio.play();
-            e.value=e.value
+            e.value = e.value
         }
         else {
             e.audio.pause();
-              outputUpdate(100)
+            outputUpdate(100)
         }
     })
 }
@@ -43,7 +44,9 @@ const start = (names) => {
         }
     })
     startAudio();
-  
+    chengebg(names)
+    range(names)
+
 }
 const btn = document.querySelectorAll('button')
 btn.forEach(e => e.addEventListener('click', (e) => {
@@ -53,12 +56,25 @@ btn.forEach(e => e.addEventListener('click', (e) => {
 
 
 
-function outputUpdate(vol){
-    console.log(vol);
-soundData.forEach(e=>{
-    e.audio.volume=vol/100;
-})
+function outputUpdate(vol) {
+   soundData.forEach(e => {
+        e.audio.volume = vol / 100;
+    })
 }
 
-let inout=document.querySelector('input').value
-console.log(inout);
+let inout = document.querySelector('input').value
+const chengebg = (url) => {
+    document.querySelector('.blur').style.backgroundImage = `url('files/assets/${url}-bg.jpg')`
+}
+
+
+const range = (names) => {
+    let inpt = document.querySelectorAll('.range')
+    let index = soundData.findIndex(e => e.name == names)
+    inpt.forEach(e => {
+        e.style.display = 'none'
+    })
+    inpt[index].style.display = 'block'
+}
+
+
