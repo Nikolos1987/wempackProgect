0c4780db9e75c543a7ad616505213c707fb5ebc7
nikita <nikolos04051987@gmail.com>
2023-10-14 20:02:29 +0700

    fix:addwebpack

--- index.js
+++ index.js
@@ -44,30 +44,24 @@ const start = (names) => {
         }
     })
     startAudio();
-    chengebg(names)
-    range(names)
-
+    chengebg(names);
+    range(names);
 }
-const btn = document.querySelectorAll('button')
+const btn = document.querySelectorAll('button');
 btn.forEach(e => e.addEventListener('click', (e) => {
     let name = e.target.dataset.name;
     start(name);
 }))
-
-
-
 function outputUpdate(vol) {
-   soundData.forEach(e => {
+    soundData.forEach(e => {
         e.audio.volume = vol / 100;
-    })
+    });
 }
 
 let inout = document.querySelector('input').value
 const chengebg = (url) => {
     document.querySelector('.blur').style.backgroundImage = `url('files/assets/${url}-bg.jpg')`
 }
-
-
 const range = (names) => {
     let inpt = document.querySelectorAll('.range')
     let index = soundData.findIndex(e => e.name == names)
